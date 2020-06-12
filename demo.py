1c360a71a3ed82f074b28bee8b73ea570a704c28
ShileiWu <shileiwu@human-horizons.com>
2020-06-12 16:07:44 +0800

    power_on_off

--- demo.py
+++ demo.py
@@ -16,6 +16,10 @@ import serial
 import serial.tools.list_ports
 
 
+def hex_to_str(s):
+    return ''.join([chr(i) for i in [int(b, 16) for b in s.split(r'/x')[1:]]])
+
+
 def portlist():
     plist = list(serial.tools.list_ports.comports())
 
@@ -105,8 +109,41 @@ def portwrite16(port, bps, arg):
         # 写数据
         result = ser.write(arg)
         print("写总字节数：", result)
-        # b = b"\x3c\x07\x01\x03\x03\x12\x20"
-        # ser.write(b)
+        if ser.read(6).hex() == '3a0602030106':
+            print("Successful")
+        else:
+            print("Failed")
+
+        ser.close()  # 关闭串口
+
+    except Exception as e:
+        print("error!", e)
+
+
+def portpower16(port, bps, arg):
+    try:
+        # 端口：CNU； Linux上的/dev /ttyUSB0等； windows上的COM3等
+        portx = port
+
+        # 波特率，标准值有：50,75,110,134,150,200,300,600,1200,1800,2400,4800,9600,19200,38400,57600,115200
+        bps = bps
+
+        # 超时设置，None：永远等待操作；
+        #         0：立即返回请求结果；
+        #        其他：等待超时时间（单位为秒）
+        timex = 10
+
+        # 打开串口，并得到串口对象
+        ser = serial.Serial(portx, bps, timeout=timex)
+
+        # 写数据
+        result = ser.write(arg)
+        print("写总字节数：", result)
+        # print(ser.read(6).hex())
+        if ser.read(6).hex() == '3a0602040107':
+            print("Successful")
+        else:
+            print("Failed")
 
         ser.close()  # 关闭串口
 
@@ -116,13 +153,24 @@ def portwrite16(port, bps, arg):
 
 if __name__ == "__main__":
     port = portlist()  # 获取串口号
-    # print(port)
     bps = 115200  # 设定波特率
-    # connect_pc = chr(0x3C)   #chr(0x08), chr(0x01), chr(0x03), chr(0x02), chr(0x01), chr(0x02), chr(0x11)
-    power_on = [0x3C, 0x08, 0x01, 0x03, 0x02, 0x01, 0x02, 0x09]
+    usb_power_on = [0x3C, 0x08, 0x01, 0x03, 0x02, 0x01, 0x02, 0x09]
     connect_pc = [0x3C, 0x07, 0x01, 0x03, 0x03, 0x12, 0x19]
-    portwrite16(port, bps, arg=power_on)
-    sleep(5)
+    connect_B_1 = [0x3C, 0x07, 0x01, 0x03, 0x01, 0x12, 0x17]
+    connect_B_2 = [0x3C, 0x07, 0x01, 0x03, 0x03, 0x11, 0x18]
+    usb_switch_check = [0x3A, 0x06, 0x02, 0x03, 0x01, 0x06]  # 3a0602030106
+    power_on = [0x3C, 0x08, 0x01, 0x04, 0x03, 0x01, 0x01, 0x0A]
+    power_off = [0x3C, 0x08, 0x01, 0x04, 0x03, 0x01, 0x00, 0x09]
+    power_check = [0x3A, 0x06, 0x02, 0x04, 0x01, 0x07]  # 3a0602040107
+
+    portwrite16(port, bps, arg=usb_power_on)
+    sleep(2)
     portwrite16(port, bps, arg=connect_pc)
-    # readport(port_list)
-    # portwrite(connect_pc)
+    sleep(2)
+    portwrite16(port, bps, arg=connect_B_1)
+    sleep(2)
+    portwrite16(port, bps, arg=connect_B_2)
+    sleep(5)
+    portpower16(port, bps, arg=power_on)
+    sleep(5)
+    portpower16(port, bps, arg=power_off)
